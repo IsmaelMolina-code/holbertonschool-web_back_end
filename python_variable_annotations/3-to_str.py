78123e28fff215646a7cff29fc64bbad78248281
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-09-02 12:05:21 -0300

    Task 3 of Python - Variable Annotations

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Function that takes a float and returns a string """
+
+
+def to_str(n: float) -> str:
+    """ Returning the string representation of a float """
+    return str(n)
