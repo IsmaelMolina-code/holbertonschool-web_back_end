9c8d7d0a42b159696ebc4101450689ab45f72184
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-09-02 11:47:52 -0300

    Task 0 of Python - Variable Annotations

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,6 +2,6 @@
 """ Add two floats together and return the result. """
 
 
-def add (a: float, b: float) -> float:
+def add(a: float, b: float) -> float:
     """ Function to add them """
-    return(a + b)
+    return (a + b)
