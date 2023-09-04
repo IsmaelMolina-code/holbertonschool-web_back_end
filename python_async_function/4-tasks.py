7d2b24115cd0f4096d8ea06e8b735ea824157c81
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-09-03 23:46:29 -0300

    Task 4 of Python - Async Functions

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Asyncio basics """
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ Returns a list of all the delays (float values) """
+    delays: List[float] = []
+    for _ in range(n):
+        delays.append(await task_wait_random(max_delay))
+    return sorted(delays)
+
+
