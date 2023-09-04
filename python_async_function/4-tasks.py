32beb44e2c49dd3f1568e1e3e0eda54272f2ff06
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-09-03 23:50:38 -0300

    Task 4 of Python - Async Functions

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -6,6 +6,7 @@ from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ Returns a list of all the delays (float values) """
     delays: List[float] = []
