23ad75bc2d47e3a402fbc49ed07f9db392d1a664
root <ismaelmolina544@gmail.com>
2023-09-05 11:33:34 -0700

    Task 9 of NoSQL

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-
+"""This module contains functions for inserting data into the database"""
 
 def insert_school(mongo_collection, **kwargs):
+    """Inserts a school into the database"""
     mongo_collection.insert_one(kwargs)
     return (kwargs.get(u'_id'))
