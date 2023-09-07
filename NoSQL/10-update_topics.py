a03c7d192154dcd506594a7b363d9a8c6e19bafa
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-09-07 17:19:52 -0300

    Task 10 of NoSQL

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ This module contains functions for updating data in the database """
 
+
 def update_topics(mongo_collection, name, topics):
     """Updates a school's topics"""
     mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
