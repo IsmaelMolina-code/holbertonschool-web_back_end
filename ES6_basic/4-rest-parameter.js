a697184204ea71242951716449bbd30415b94bf5
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-09-17 18:45:27 -0300

    Task 4 of ES6

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,3 +1,6 @@
-export default function returnHowManyArguments() {
-  return arguments.length;
+export default function returnHowManyArguments(...args) {
+  let total = 0;
+  args.forEach((arg) => {
+    total += 1; });
+    return total;
 }
