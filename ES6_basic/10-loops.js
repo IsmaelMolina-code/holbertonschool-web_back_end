8f928f352aedeb44a40c6e8b2816c611c3afb1e4
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-09-17 19:58:04 -0300

    Task 10 of ES6

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array.keys()) {
-    array[idx] = appendString + array[idx];
+  const newArr = [];
+  for (const idx of array) {
+    newArr.push(appendString + idx);
   }
-
-  return array;
+  return newArr;
 }
